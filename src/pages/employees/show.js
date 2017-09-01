caa5100666c948c895a6bfd22025a47d3875dac7
Sten Nilsen <sten@vangenplotz.no>
2017-09-01 18:36:01 +0200

    single employee wip

--- src/pages/employees/show.js
+++ src/pages/employees/show.js
@@ -1,5 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Container, Card, Icon, Image } from 'semantic-ui-react'
+import Navigation from "../../containers/navigation/navigation"
 
 export default class Show extends React.Component {
 
@@ -9,7 +11,31 @@ export default class Show extends React.Component {
 
 	render() {
 		return <div>
+			<Navigation />
+			<Container>
+			<Card>
+				<Card.Content>
+					<Card.Header>
+						Matthew
+					</Card.Header>
+					<Card.Meta>
+        <span className='date'>
+          Joined in 2015
+        </span>
+					</Card.Meta>
+					<Card.Description>
+						Matthew is a musician living in Nashville.
+					</Card.Description>
+				</Card.Content>
+				<Card.Content extra>
+					<a>
+						<Icon name='user' />
+						22 Friends
+					</a>
+				</Card.Content>
+			</Card>
 			{JSON.stringify(this.props.employee)}
+			</Container>
 		</div>
 	}
 }
