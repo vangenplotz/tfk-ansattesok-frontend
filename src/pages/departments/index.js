8a81ea177a5c13db212458cd4ea5ceabe4fb3090
Espen Rønning <espen@vangenplotz.no>
2017-09-01 19:14:54 +0200

    Refactored scss. Footer got own .scss

--- src/pages/departments/index.js
+++ src/pages/departments/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Table, Icon } from "semantic-ui-react"
+import { Container, Grid, Icon, Table } from "semantic-ui-react"
 import { Link } from "react-router"
 import Navigation from "../../containers/navigation/navigation"
 
@@ -11,7 +11,6 @@ export default class Index extends React.Component {
 
 	renderBody() {
 		return this.props.departments
-				.filter(department => !!department.name)
 				.map(department => <Table.Row key={department.id}>
 					<Table.Cell><Link to={`/departments/${department.id}`}>{department.name}</Link></Table.Cell>
 					<Table.Cell><Icon name="mail outline" /> <a href="mailto:post@t-fk.no">post@t-fk.no</a></Table.Cell>
@@ -22,21 +21,29 @@ export default class Index extends React.Component {
 	render() {
 		return <div className="app-container">
 			<Navigation />
-			<Table>
-				<Table.Header>
-					<Table.Row>
-						<Table.HeaderCell colSpan="3">Avdelinger</Table.HeaderCell>
-					</Table.Row>
-					<Table.Row>
-						<Table.HeaderCell>Navn</Table.HeaderCell>
-						<Table.HeaderCell>E-post</Table.HeaderCell>
-						<Table.HeaderCell>Nettside</Table.HeaderCell>
-					</Table.Row>
-				</Table.Header>
-				<Table.Body>
-					{this.renderBody()}
-				</Table.Body>
-			</Table>
+			<Container>
+				<Grid columns={1}>
+					<Grid.Row>
+						<Grid.Column width={16}>
+							<Table>
+								<Table.Header>
+									<Table.Row>
+										<Table.HeaderCell colSpan="3">Avdelinger</Table.HeaderCell>
+									</Table.Row>
+									<Table.Row>
+										<Table.HeaderCell>Navn</Table.HeaderCell>
+										<Table.HeaderCell>E-post</Table.HeaderCell>
+										<Table.HeaderCell>Nettside</Table.HeaderCell>
+									</Table.Row>
+								</Table.Header>
+								<Table.Body>
+									{this.renderBody()}
+								</Table.Body>
+							</Table>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Container>
 		</div>
 	}
 }
